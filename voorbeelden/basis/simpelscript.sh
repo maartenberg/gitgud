f2ad3188f9a4dd26031990ade92d1e5640dfea9f
Maarten van den Berg <schoenveter123+git@gmail.com>
2016-02-16 13:44:03 +0100

    Nieuwe versie testscript 1

--- voorbeelden/basis/simpelscript.sh
+++ voorbeelden/basis/simpelscript.sh
@@ -1,2 +1,10 @@
 echo "Hallo `whoami`!"
 echo "Dit is een simpel script, en vandaag is het de 16e!" # fixme: vervang de 16e met `date +%d`
+
+if [ ! -f "laatstekeer.txt" ]; then
+	echo "Dit is de eerste keer dat je dit script draait!"
+else
+	echo "Dit script is voor het laatst gedraaid op `cat laatstekeer.txt`!"
+fi
+
+date -R > laatstekeer.txt
